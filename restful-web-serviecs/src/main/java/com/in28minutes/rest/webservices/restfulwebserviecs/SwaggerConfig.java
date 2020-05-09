5189c2a7064fc6b754ebe35aead8fc3f0235b2d5
Abhishek Singh <abhiwanttochat@gmail.com>
2020-05-09 21:35:07 +0530

    Configuring auto generation of Swagger documentation

---
+++ restful-web-serviecs/src/main/java/com/in28minutes/rest/webservices/restfulwebserviecs/SwaggerConfig.java
@@ -0,0 +1,43 @@
+package com.in28minutes.rest.webservices.restfulwebserviecs;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Primary;
+import org.springframework.hateoas.client.LinkDiscoverer;
+import org.springframework.hateoas.client.LinkDiscoverers;
+import org.springframework.hateoas.mediatype.collectionjson.CollectionJsonLinkDiscoverer;
+import org.springframework.plugin.core.SimplePluginRegistry;
+
+import com.google.common.net.MediaType;
+
+import springfox.documentation.spi.DocumentationType;
+import springfox.documentation.spring.web.plugins.Docket;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
+
+//Configuration
+@Configuration
+//Enable Swagger
+@EnableSwagger2
+public class SwaggerConfig {
+	//Bean - Docket
+	
+	@Bean
+	public Docket api() {
+		return new Docket(DocumentationType.SWAGGER_2);
+	}
+	
+
+	@Bean
+	public LinkDiscoverers discoverers() {
+	    List<LinkDiscoverer> plugins = new ArrayList<>();
+	    plugins.add(new CollectionJsonLinkDiscoverer());
+	    return new LinkDiscoverers(SimplePluginRegistry.create(plugins));
+	}
+	
+	//Swagger2
+	//All the paths
+	//All the APIs
+}
