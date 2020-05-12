81e4e3c839b11855e1c658c1b99d88dc1e214a74
Abhishek Singh <abhiwanttochat@gmail.com>
2020-05-12 12:09:58 +0530

    Versioning RESTful services using Media Type

--- restful-web-serviecs/src/main/java/com/in28minutes/rest/webservices/restfulwebserviecs/versioning/PersonVersioningController.java
+++ restful-web-serviecs/src/main/java/com/in28minutes/rest/webservices/restfulwebserviecs/versioning/PersonVersioningController.java
@@ -17,14 +17,35 @@ public class PersonVersioningController {
 		return new PersonV2(new Name("Brian", "Lara"));
 	}
 	
+	@GetMapping(value="person/param", params = "version=1")
+	public PersonV1  paramV1() {
+		return new PersonV1("Rahul Dravid");
+	}
+	
+	@GetMapping(value="person/param", params ="version=2")
+	public PersonV2 paramV2() {
+		return new PersonV2(new Name("Brian", "Lara"));
+	}
+	
+	
 	@GetMapping(value="person/header", headers = "X-API-VERSION=1")
 	public PersonV1  headerV1() {
 		return new PersonV1("Rahul Dravid");
 	}
 	
-	@GetMapping(value="person/header", headers ="X-API-VERSION=2")
+	@GetMapping(value="person/header", headers  ="X-API-VERSION=2")
 	public PersonV2 headerV2() {
 		return new PersonV2(new Name("Brian", "Lara"));
 	}
 
+	
+	@GetMapping(value="person/produces", produces = "application/vnd.company.appV1+json")
+	public PersonV1  producesV1() {
+		return new PersonV1("Rahul Dravid");
+	}
+	
+	@GetMapping(value="person/produces", produces  ="application/vnd.company.appV2+json")
+	public PersonV2 producesV2() {
+		return new PersonV2(new Name("Brian", "Lara"));
+	}
 }
